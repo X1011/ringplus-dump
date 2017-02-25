783a66af228d6e2a366286981d927b4c15f193fb
dan smith <XX1011@gmail.com>
2017-02-24 23:40:10 -0500

    rm unused Flask dependency and apparent draft code

--- ringplus-dump.py
+++ ringplus-dump.py
@@ -229,38 +229,6 @@ def _default_parser(data):
         return dict(parse_qsl(data))
 
 
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def homepage():
-    text = '<a href="%s">Authenticate with reddit</a>'
-    return text % make_authorization_url()
-
-def make_authorization_url():
-    # Generate a random string for the state parameter
-    # Save it for use later to prevent xsrf attacks
-    from uuid import uuid4
-    state = str(uuid4())
-    save_created_state(state)
-    params = {"client_id": CLIENT_ID,
-              "response_type": "code",
-              "state": state,
-              "redirect_uri": REDIRECT_URI,
-              "duration": "temporary",
-              "scope": "identity"}
-    import urllib
-    url = "https://ssl.reddit.com/api/v1/authorize?" + urllib.urlencode(params)
-    return url
-
-
-'''
-# redirect user to authorization page
-my_redirect(client.flow.authorization_uri(state=my_state))
-
-# get access token and make a resource request
-c.request_token(response_dict)
-'''
-
 
 import webbrowser
 from pprint import pprint
